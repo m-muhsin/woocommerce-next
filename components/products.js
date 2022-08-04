5de5408540470cc44b89e96ee13f0e81bb74adf5
Muhammad Muhsin <muhammad.muhseen@gmail.com>
2022-08-04 12:31:41 +0530

    Show products without a price last, strikethrough regular_price if regular price more than price.

--- components/products.js
+++ components/products.js
@@ -25,23 +25,60 @@ const Price = styled.div`
   font-size: 1em;
 `;
 
-const Products = ({ products = [] }) => (
-  <Section>
-    {products.map(product => (
-      <Single key={product.id}>
-        <Link href={`/products/${product.slug}`}>
-          <a>
-            <Img
-              src={product.images[0].src}
-              alt={`${product.name} featured image`}
-            />
-            <Title>{product.name}</Title>
-            <Price>$ {product.price}</Price>
-          </a>
-        </Link>
-      </Single>
-    ))}
-  </Section>
-);
+const RegularPrice = styled.span`
+  text-decoration: line-through
+`;
+
+const Products = ({ products = [] }) => {
+
+  // Products without a price should go last.
+  products = products.sort((a, b) => {
+    if (a.price && !b.price) {
+      return -1;
+    } else if (!a.price && b.price) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+
+  return (
+    <Section>
+      {products.map(product => (
+        <Single key={product.id}>
+          <Link href={`/products/${product.slug}`}>
+            <a>
+              <Img
+                src={product.images[0].src}
+                alt={`${product.name} featured image`}
+              />
+              <Title>{product.name}</Title>
+              {
+                product.price && (
+                  <Price>
+                    {product.price && product.regular_price &&
+                      product.price < product.regular_price ?
+                      (
+                        <>
+                          <RegularPrice>
+                            ${product.regular_price}
+                          </RegularPrice>{' '}
+                          <span>
+                            ${product.price}
+                          </span>
+                        </>
+                      ) : (
+                        <span>${product.price}</span>
+                      )}
+                  </Price>
+                )
+              }
+            </a>
+          </Link>
+        </Single>
+      ))}
+    </Section>
+  )
+}
 
 export default Products;
